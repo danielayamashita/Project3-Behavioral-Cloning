14c53f942ecb105f4fe1292f4d65e8b636a13b60
Daniela <daniyamashita@gmail.com>
2018-03-20 21:40:52 -0300

    master: Adicionado a segunda versão do projeto3

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ import numpy as np
 import os.path
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda ,Cropping2D
+from keras.layers import Flatten, Dense, Lambda ,Cropping2D,Dropout
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.models import load_model
@@ -21,7 +21,7 @@ from keras.models import load_model
 # Unzip the file containing all the images
 from zipfile import ZipFile
 # specifying the zip file name
-file_name = "../Teste5.zip"
+file_name = "../Teste6.zip"
  
 # opening the zip file in READ mode
 with ZipFile(file_name, 'r') as zap:
@@ -37,20 +37,17 @@ with ZipFile(file_name, 'r') as zap:
 #----------------- IMPORT THE DATA --------------------------------------------
 #==============================================================================
 lines = []
-with open('Teste4/driving_log.csv') as csvfile:
+with open('Teste6/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
     
-
-
-
 numLines = 0
 images = []
 measurements = []
-
 for line in lines:
     numLines= numLines+1
+    #center images
     source_path = line[0]
     filename = source_path.split('\\')[-1]
     current_path = 'Teste4/IMG/' +filename
@@ -58,11 +55,13 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
-    if (numLines >9000):
+
+    if (numLines >10000):
         break
-		
-		
-augmented_images = []
+
+#==============================================================================
+#----------------- DATA AUGMENTATION ------------------------------------------
+#==============================================================================
 augmented_measurements = []
 for image, measurement  in zip(images,measurements):
     augmented_images.append(image)
@@ -70,14 +69,29 @@ for image, measurement  in zip(images,measurements):
     augmented_images.append(cv2.flip(image,1))
     augmented_measurements.append(measurement*-1.0)
 	
+
+#Split the data into training and validating dat
+total = len(augmented_measurements)
+n_train= np.floor(total*0.8).astype(int)
+
+X_train, y_train = np.array(augmented_images[0:n_train]), np.array( augmented_measurements[0:n_train])
+X_test, y_test = np.array(augmented_images[n_train+1:-1]), np.array( augmented_measurements[n_train+1:-1])
+
+#==============================================================================
+#----------------- MODEL DEFINITION -------------------------------------------
+#==============================================================================	
 model = Sequential()
 model.add(Lambda(lambda x:x/255-0.5,input_shape=(160,320,3)))
 model.add(Cropping2D(cropping =((70,25),(0,0))))
+model.add(Convolution2D(12,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,2,2,activation='relu'))
+model.add(Convolution2D(64,2,2,activation='relu'))
+model.add(Convolution2D(64,2,2,activation='relu'))
+
+model.add(MaxPooling2D((2,2)))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -85,9 +99,29 @@ model.add(Dense(10))
 model.add(Dense(1))
 model.compile(loss ='mse', optimizer = 'adam')
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
 
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True,nb_epoch =5)
+#Split the data into training and validating data
+total = len(augmented_measurements)
+n_train= np.floor(total*0.9).astype(int)
+X_train, y_train = np.array(augmented_images[0:n_train]), np.array(augmented_measurements[0:n_train])
+X_test, y_test = np.array(augmented_images[n_train+1:-1]), np.array(augmented_measurements[n_train+1:-1])
 
+
+model.fit(X_train, y_train, shuffle=True, nb_epoch = 5)
+print("Finished")
 model.save('model.h5')
+#Evaluate the model
+
+print("Testing")
+metrics = model.evaluate( x=X_test, y=y_test)
+
+for metric_i in range(len(model.metrics_names)):
+    metric_name = model.metrics_names[metric_i]
+    if metrics.size ==1:
+        metric_value  = metrics
+    else:
+        metric_value = metrics[metric_i]    
+    print('{}: {}'.format(metric_name, metric_value))
+    
+model.save('model2.h5')
+
